e1d8015355a3d761b9349fb94f95f19e1b1bbded
SiNiStErsamandar <abdumannopov.saman@gmail.com>
2022-01-19 12:37:48 +0500

    bmi result

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ while True:
     weight = float(input("Sizning vazningiz kg: "))
 
     bmi = weight / (height ** 2)
+    bmi = round(bmi, 1)
 
     if bmi < 18.5:
-        print(str(bmi) + " Vazningiz kam")
+        print(f"BMI {bmi} Vazningiz kam")
     elif bmi > 18.5 and bmi < 24.9:
-        print(str(bmi) + " Vazningiz o'rtacha")
+        print(f"BMI {bmi} Vazningiz o'rtacha")
     elif bmi > 25.0 and bmi < 29.9:
-        print(str(bmi) + " Vazningiz o'rtiqcha")
+        print(f"BMI {bmi} Vazningiz o'rtiqcha")
     elif bmi == 30.0:
-        print(str(bmi) + " Vazningiz semizlik darajasida")
+        print(f"BMI {bmi} Vazningiz semizlik darajasida")
     else:
-        print(str(bmi) + " Siz Filsiz !!!")  
+        print(f"BMI {bmi} Siz Filsiz !!!")  
+
+
+    # return "Your BMI is " + format(bmi, ".1f") + "(Obesity)"
