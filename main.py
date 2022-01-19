2f4a27f46101192debd679d6341290c06dcab6a7
SiNiStErsamandar <abdumannopov.saman@gmail.com>
2022-01-19 10:51:06 +0500

    added main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ while True:
     bmi = weight / (height ** 2)
 
     if bmi < 18.5:
-        print("Vazningiz kam")
+        print(str(bmi) + " Vazningiz kam")
     elif bmi > 18.5 and bmi < 24.9:
-        print("Vazningiz o'rtacha")
+        print(str(bmi) + " Vazningiz o'rtacha")
     elif bmi > 25.0 and bmi < 29.9:
-        print("Vazningiz ortiqcha")
+        print(str(bmi) + " Vazningiz o'rtiqcha")
     elif bmi == 30.0:
-        print("Vazningiz semizlik darajasida")
+        print(str(bmi) + " Vazningiz semizlik darajasida")
     else:
-        print("Siz Filsiz !!!")  
+        print(str(bmi) + " Siz Filsiz !!!")  
